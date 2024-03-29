3877f5099a5bcef0d50e27862f8f6554c5553126
James Sinnott <JamesRichardSinnott@gmail.com>
2024-03-29 16:53:25 +0000

    [ADD] Projecting content into components with ng-content

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,15 +10,20 @@ export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output("bpCreated") blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', { static: true }) serverContentInput;
 
   onAddServer(nameInput: HTMLInputElement) {
-    console.log(nameInput);
-    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    console.log(this.serverContentInput);
+    this.serverCreated.emit(
+      {
+        serverName: nameInput.value, 
+        serverContent: this.serverContentInput.nativeElement.value
+      });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
-    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent})
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value})
   }
 }
