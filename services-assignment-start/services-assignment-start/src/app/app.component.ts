2bbc67ffbab5b21a1577eff2cec06a3671a2871f
James Sinnott <JamesRichardSinnott@gmail.com>
2024-04-15 16:54:55 +0100

    [ADD] Services assignment complete

---
+++ services-assignment-start/services-assignment-start/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { UsersService } from './users.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [UsersService] // Available at component level
+})
+export class AppComponent {
+}
